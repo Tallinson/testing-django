d0656df8b9f1e624d0d21ad73a68787b5226e5b7
Milinches <ibrogenius5165@gmail.com>
2021-01-27 22:06:26 +0100

    added new files

--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('', views.home),
     path('about/', views.about),
-
 ]
+
+
+
+
