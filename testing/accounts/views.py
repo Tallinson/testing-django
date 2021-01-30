97024abb6cf9f3210a309d6b0eff693c9a6c7f9c
Milinches <ibrogenius5165@gmail.com>
2021-01-30 23:51:35 +0100

    Validation

--- testing/accounts/views.py
+++ testing/accounts/views.py
@@ -18,7 +18,10 @@ def signup_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # login
+            return redirect('articles:article_list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
