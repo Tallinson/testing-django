29753623e9281c8b61edb81fc47de0cc9ce04ee3
Milinches <ibrogenius5165@gmail.com>
2021-01-31 12:28:44 +0100

    Authenticating Login

--- testing/accounts/views.py
+++ testing/accounts/views.py
@@ -9,7 +9,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
 
             return redirect('articles:article_list')
     else:
